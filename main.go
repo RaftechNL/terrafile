f2f51137ff2b984519fdbaff61c72c0698c3c4a9
Rafal P | Raftech <rafal.pieniazek@raftech.nl>
2023-01-10 17:35:26 +0100

    initial cleanup for Github ops

--- main.go
+++ main.go
@@ -1,101 +1,62 @@
-/*
-Copyright 2022 IDT Corp.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"sync"
 
-	"github.com/jessevdk/go-flags"
-	"github.com/nritholtz/stdemuxerhook"
-	log "github.com/sirupsen/logrus"
-	"gopkg.in/yaml.v2"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 )
-
-type module struct {
-	Source  string `yaml:"source"`
-	Version string `yaml:"version"`
-}
-
-var opts struct {
-	ModulePath string `short:"p" long:"module_path" default:"./vendor/modules" description:"File path to install generated terraform modules"`
-
-	TerrafilePath string `short:"f" long:"terrafile_file" default:"./Terrafile" description:"File path to the Terrafile file"`
-}
-
-// To be set by goreleaser on build
-var (
-	version = "dev"
-	commit  = "none"
-	date    = "unknown"
-)
-
-func init() {
-	// Needed to redirect logrus to proper stream STDOUT vs STDERR
-	log.AddHook(stdemuxerhook.New(log.StandardLogger()))
-}
-
-func gitClone(repository string, version string, moduleName string) {
-	log.Printf("[*] Checking out %s of %s \n", version, repository)
-	cmd := exec.Command("git", "clone", "--single-branch", "--depth=1", "-b", version, repository, moduleName)
-	cmd.Dir = opts.ModulePath
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
 
 func main() {
-	fmt.Printf("Terrafile: version %v, commit %v, built at %v \n", version, commit, date)
-	_, err := flags.Parse(&opts)
+	// Get the repository URL and authentication method from the command line arguments
+	url := os.Args[1]
+	authMethod := os.Args[2]
 
-	// Invalid choice
-	if err != nil {
+	var auth transport.AuthMethod
+
+	// Set the appropriate authentication method
+	if authMethod == "ssh" {
+		privateKey, err := ioutil.ReadFile(os.Getenv("HOME") + "/.ssh/id_rsa")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		signer, err := ssh.NewSignerFromKey(privateKey)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		auth = &ssh.PublicKeys{User: "git", Signer: signer}
+	} else if authMethod == "token" {
+		token := os.Getenv("GITHUB_TOKEN")
+		if token == "" {
+			fmt.Println("GITHUB_TOKEN environment variable is not set")
+			os.Exit(1)
+		}
+		auth = &http.BasicAuth{
+			Username: "x-access-token",
+			Password: token,
+		}
+	} else {
+		fmt.Printf("Invalid authentication method: %s\n", authMethod)
 		os.Exit(1)
 	}
 
-	// Read File
-	yamlFile, err := ioutil.ReadFile(opts.TerrafilePath)
+	// Clone the repository
+	_, err := git.PlainClone(os.TempDir()+"/repo", false, &git.CloneOptions{
+		URL:           url,
+		Auth:          auth,
+		ReferenceName: plumbing.ReferenceName("refs/heads/master"),
+		SingleBranch:  true,
+	})
+
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
-
-	// Parse File
-	var config map[string]module
-	if err := yaml.Unmarshal(yamlFile, &config); err != nil {
-		log.Fatalln(err)
-	}
-
-	// Clone modules
-	var wg sync.WaitGroup
-	_ = os.RemoveAll(opts.ModulePath)
-	_ = os.MkdirAll(opts.ModulePath, os.ModePerm)
-	for key, mod := range config {
-		wg.Add(1)
-		go func(m module, key string) {
-			defer wg.Done()
-			gitClone(m.Source, m.Version, key)
-			_ = os.RemoveAll(filepath.Join(opts.ModulePath, key, ".git"))
-		}(mod, key)
-	}
-
-	wg.Wait()
 }

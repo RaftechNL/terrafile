3b3ae338cd1d54802b4faa6520e163ea8c529ef0
Rafal P | Raftech <rafal.pieniazek@raftech.nl>
2023-01-29 11:55:05 +0100

    update main with changes

--- main.go
+++ main.go
@@ -3,16 +3,26 @@ package main
 import (
 	"fmt"
 
+	_ "github.com/RaftechNL/terrafile/internal/providers/gh"
 	"github.com/RaftechNL/terrafile/internal/terrafilev2"
 )
 
 func main() {
 
-	tfv2, errv2 := terrafilev2.ReadFromYAML("v2.yaml")
+	tfv2, errv2 := terrafilev2.InitTerrafileV2FromYAML("v2.yaml")
 	if errv2 != nil {
 		fmt.Println(errv2)
 	}
 
-	fmt.Println(tfv2)
+	for _, module := range tfv2.Modules {
+		fmt.Println(module.Source)
+		// x := tfv2.Providers[module.ProviderAliasRef]
+
+		// fmt.Println(x)
+
+		// module.Provider.DownloadModule(&module.ProviderConfig)
+
+		fmt.Println(module)
+	}
 
 }

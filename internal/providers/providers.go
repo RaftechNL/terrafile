43dea9fb66e56efab908a118131f145cb18781f6
Rafal P | Raftech <rafal.pieniazek@raftech.nl>
2023-01-29 23:33:12 +0100

    adds dedicated structure for ModuleSpec

--- internal/providers/providers.go
+++ internal/providers/providers.go
@@ -1,5 +1,11 @@
 package providers
 
+// ModuleSpec is used to deliver details about the module to be downloaded into the provider
+type ModuleSpec struct {
+	Source  string `yaml:"source"`
+	Version string `yaml:"version"`
+}
+
 type ProviderIface interface {
-	DownloadModule() error
+	DownloadModule(moduleSpec ModuleSpec, outputPath string) error
 }
